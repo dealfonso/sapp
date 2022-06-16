fe9f756f1382dadca557fb0a1d59e93c473e0a29
Carlos A <caralla@upv.es>
2022-06-16 14:00:21 +0200

    support for escaped parenthesis

--- src/PDFObjectParser.php
+++ src/PDFObjectParser.php
@@ -217,19 +217,21 @@
             $n_parenthesis = 1;
             while ($this->_c !== false) {
                 $this->nextchar();
-                if ($this->_c === ')') {
+                if (($this->_c === ')') && ($token[strlen($token) - 1] !== '\\')) {
                     $n_parenthesis--;
                     if ($n_parenthesis == 0)
                         break;
                 } else {
-                    if ($this->_c === '(') {
+                    if (($this->_c === '(') && ($token[strlen($token) - 1] !== '\\')) {
                         $n_parenthesis++;
                     }
                     $token .= $this->_c;
                 }
             }
 
-            if ($this->_c !== ")") throw new Exception("Invalid string");
+            if ($this->_c !== ")") {
+                throw new Exception("Invalid string");
+            }
             $this->nextchar();
 
             return $token;
