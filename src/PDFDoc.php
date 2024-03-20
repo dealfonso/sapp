9df350ff24eee128bb8f3a77cb495802f61e60dc
M Hida <40340343+hidasw@users.noreply.github.com>
2024-03-20 22:54:00 +0700

    Add last parameter for chain certs
    Add last parameter for chain certs

--- src/PDFDoc.php
+++ src/PDFDoc.php
@@ -799,6 +799,7 @@ class PDFDoc extends Buffer {
 
             // Calculate the signature and remove the temporary file
             $certificate = $_signature->get_certificate();
+            // Add last parameter for chain certs
             $signature_contents = PDFUtilFnc::calculate_pkcs7_signature($temp_filename, $certificate['cert'], $certificate['pkey'], __TMP_FOLDER, $certificate['extracerts']);
             unlink($temp_filename);
 
